e0928aa6b6d050645fb90b7340446f703eabda28
Damien Katz <damien@couchbase.com>
2013-03-11 18:05:30 -0700

    better error handling

--- memhog.c
+++ memhog.c
@@ -1,12 +1,17 @@
 #include <sys/mman.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, const char* argv[]) {
 	char* lastchar;
 	long bytes_to_hog;
 	void* mem;
-	if (argv[1][0] == '0' && (argv[1][1] == 'x' || argv[1][1] == 'X')) {
+	if (argc != 2) {
+		printf("Failed: Missing argument, make sure to pass a base 10 or 16 integer (ex: 255 or 0xFF)\n");
+		return 1;
+	}
+	if (strstr(argv[1], "0x") == argv[1]) {
 		/* base 16 */
 		bytes_to_hog = strtol(argv[1], &lastchar, 16);
 	} else {
@@ -14,7 +19,7 @@ int main(int argc, const char* argv[]) {
 		bytes_to_hog = strtol(argv[1], &lastchar, 10);
 	}
 	if (*lastchar != '\0') {
-		printf("Invalid argument %s, make sure to pass a base 10 or 16 integer (ex: 255 or 0xFF)\n", argv[1]);
+		printf("Failed: Invalid argument %s, make sure to pass a base 10 or 16 integer (ex: 255 or 0xFF)\n", argv[1]);
 		return 1;
 	}
 	printf("Attempting to hog %li bytes of RAM.\n", bytes_to_hog);
